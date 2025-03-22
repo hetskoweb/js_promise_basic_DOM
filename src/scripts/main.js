1fe29020a1d9ae56ac96356881fe1cc809552af6
Illia <hetsko.ilya@gmail.com>
2025-03-22 20:45:50 +0200

    fix task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,17 +8,28 @@ message.classList.add('message');
 
 const promise1 = new Promise((resolve, reject) => {
   logo.addEventListener('click', () => {
-    resolve('Promise was resolved');
+    resolve('Promise was resolved!');
   });
 });
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reject(new Error('Promise was rejected'));
+    reject(new Error('Promise was rejected!'));
   }, 3000);
 });
 
 function logoClick() {
-  Promise.race([promise1, promise2])
+  promise1
+    .then((result) => {
+      message.textContent = result;
+      document.body.appendChild(message);
+    })
+    .catch((error) => {
+      message.textContent = error.message;
+      message.classList.add('error-message');
+      document.body.appendChild(message);
+    });
+
+  promise2
     .then((result) => {
       message.textContent = result;
       document.body.appendChild(message);
